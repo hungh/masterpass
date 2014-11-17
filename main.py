d7c763329653776e61f0e2b562678e88a9e7e70e
h.huynh <h.huynh@tmcbonds.com>
2014-11-17 10:18:51 -0500

    Move main.py to 1 upper level

---
+++ main.py
@@ -0,0 +1,31 @@
+from master.handler.main_server import MainHttpServer
+from master.boostrap import initialize
+from master.consts import DEFAULT_PORT
+import sys
+
+
+def main():
+    """
+    Beta version of a Simple HTTP server
+       by 'Hung Huynh'
+    """
+    binding_port = DEFAULT_PORT
+    if len(sys.argv) == 2:
+        binding_port = int(sys.argv[1])
+    else:
+        print('Port is not provided, defaulting to ' + str(DEFAULT_PORT))
+
+    initialize()
+    MainHttpServer.start(binding_port)
+    print("Termination is in progress. Please wait...")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
