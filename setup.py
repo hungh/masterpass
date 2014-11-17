130fda2ebd0f7970ac90bc65ac5df6f28b55daef
h.huynh <h.huynh@tmcbonds.com>
2014-11-17 13:45:38 -0500

    Change Setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 
 from distutils.core import setup
 
-setup(name='MasterPswrd',
+setup(name='masterpass',
       version='1.0',
-      description='Python3 distribution script for Master password',
+      description='Python3 distribution script for Master Password',
       author='Hung Huynh',
-      author_email='h.huynh@tmcbonds.com',
-      url='http://www.tmcbonds.com',
+      author_email='hungcs@gmail.com,
+      url='http://www.apache.org',
       platforms='Linux',
       install_requires=['pymongo', 'py-bcrypt', ' pycrypto'],
       packages=['master', 'master.handler', 'master.beans', 'master.handler', 'master.httpcontroller',
