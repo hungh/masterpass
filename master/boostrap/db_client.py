6bf96a27fca28a45a822079d36db8e85dcedb4f5
h.huynh <h.huynh@tmcbonds.com>
2014-11-19 14:45:40 -0500

    Change DB_SERVER_HOST

--- master/boostrap/db_client.py
+++ master/boostrap/db_client.py
@@ -1,12 +1,12 @@
 from master.meta.singleton import Singleton
-from master.consts import DB_SERVER_PORT
+from master.consts import DB_SERVER_PORT, DB_SERVER_HOST
 from pymongo import MongoClient
 
 
 class SingleDBClient(metaclass=Singleton):
     def __init__(self):
         print('Creating MongoDB Client ')
-        self._client = MongoClient('localhost', DB_SERVER_PORT)
+        self._client = MongoClient(DB_SERVER_HOST, DB_SERVER_PORT)
 
     def get_client(self):
         return self._client
